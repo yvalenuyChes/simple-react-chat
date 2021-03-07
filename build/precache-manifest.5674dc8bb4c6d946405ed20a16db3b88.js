15a3e31d1dd0dfb41770388693b06b9045602616
Vlad Zaporozhets <piman131@yandex.ua>
2021-03-08 01:54:08 +0300

    add production app

---
+++ build/precache-manifest.5674dc8bb4c6d946405ed20a16db3b88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d75488193265781ba1b169779158e3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbfb68140fa8b98f9d1d",
+    "url": "/static/css/main.ea2e1bd9.chunk.css"
+  },
+  {
+    "revision": "191913fc3e812dd3147c",
+    "url": "/static/js/2.71e4bda0.chunk.js"
+  },
+  {
+    "revision": "4150a627f5138970747231fcef63f425",
+    "url": "/static/js/2.71e4bda0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbfb68140fa8b98f9d1d",
+    "url": "/static/js/main.186ba6fc.chunk.js"
+  },
+  {
+    "revision": "fc940f91462bfe95df3b",
+    "url": "/static/js/runtime-main.0eb9100c.js"
+  }
+]);
